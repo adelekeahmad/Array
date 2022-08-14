2a56596fb5962e0228e8ba3e38c5c5a17c9196d0
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-14 11:46:01 -0700

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Array;
+   class Program { 
+    static void Main(String[] Args)
+{
+        String[] userName = { "ahmad", "john", "tunde", "remi", "andrew", "faouq", "azeez" };
+        String[] password = { "come@1", "Hellloboy", "Himan*&", "hdkskjd&", "fyl;d[o0en", "justyou879@", };
+        string[] schools = new string[7];
+        schools[0] = "Oredola";
+        schools[1] = "Sanni Luba";
+        schools[2] = "University of Ibadan";
+        schools[3] = "Yejide";
+        schools[4] = "Grammar School";
+        schools[5] = "Wesley";
+        schools[6] = "Eti Itale";
+        Console.WriteLine("Hello, boy! you are from " + schools[5]);
+        Console.ReadLine();
+
+}
+}
+
